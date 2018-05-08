66a0b353204be95557cf729141809da2e830d255
Paul Paczuski <pavopax@users.noreply.github.com>
2018-05-08 13:03:52 -0700

    Update helpers.R

--- helpers.R
+++ helpers.R
@@ -6,9 +6,8 @@ names_to_upper <- function(.data) {
     .data %>% set_names(., toupper(names(.)))
 }
 
-## requires library(testthat)
-expect_unique_rows <- function(.data, id="id_col") {
-    expect_equal(nrow(.data), length(unique(.data[[id]])))
+expect_unique_rows <- function(.data, id="id_column") {
+    testthat::expect_equal(nrow(.data), length(unique(.data[[id]])))
 }
 
 get_file_from_path  <- function(path, file, suppress_coltype_msg=TRUE) {
